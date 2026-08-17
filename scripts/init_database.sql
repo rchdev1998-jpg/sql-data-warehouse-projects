6d3369b59cfdcf91d69039f116f7aa32e8df6862
rchdev1998-jpg <rchdev1998@gmail.com>
2026-08-17 10:07:30 +0800

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,42 +1,36 @@
 /*
-=============================================================
-Create Database and Schemas
-=============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
-	
-WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
+===================================
+	CREATE DATABASE AND SCHEMAS
+===================================
+Purpose:
+		To create database named 'maven_fuzzy_factory' and schemas. If the database is already exists,
+		it will set to single user and drop the database
+Warning:
+		Running this script can drop your entire data warehouse. Before running this script, make it sure
+		that you properly backup your database.
 */
-
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
+
+--Drop the database if exist
+IF EXISTS (select 1 from sys.databases where name = 'maven_fuzzy_factory')
+	BEGIN
+		ALTER DATABASE maven_fuzzy_factory SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+		DROP DATABASE maven_fuzzy_factory;
+	END
+GO	
+
+
+--Create Datbase
+CREATE DATABASE maven_fuzzy_factory;
 GO
 
--- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
-GO
 
-USE DataWarehouse;
-GO
-
--- Create Schemas
+--CREATE SCHEMAS
 CREATE SCHEMA bronze;
 GO
-
 CREATE SCHEMA silver;
 GO
-
 CREATE SCHEMA gold;
 GO
