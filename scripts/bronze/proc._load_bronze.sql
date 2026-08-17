ed44e3677df749e0eebb59491c4d1ea7524c8067
rchdev1998-jpg <rchdev1998@gmail.com>
2026-08-17 10:20:29 +0800

    Update proc._load_bronze.sql

--- scripts/bronze/proc._load_bronze.sql
+++ scripts/bronze/proc._load_bronze.sql
@@ -1,141 +1,181 @@
 /*
-===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
-===============================================================================
-Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
-    It performs the following actions:
-    - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
-
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
-Usage Example:
-    EXEC bronze.load_bronze;
-===============================================================================
+===================================
+	BRONZE: TRUNCATE AND INSERT
+===================================
+Purpose:
+		This is to truncate the data if exist. Full load using bulk insert.
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
-	BEGIN TRY
-		SET @batch_start_time = GETDATE();
-		PRINT '================================================';
-		PRINT 'Loading Bronze Layer';
-		PRINT '================================================';
+BEGIN TRY
+	BEGIN TRAN
+		DECLARE @startdate DATE, @enddate DATE, @batchstartdate DATE, @batchenddate DATE;
 
-		PRINT '------------------------------------------------';
-		PRINT 'Loading CRM Tables';
-		PRINT '------------------------------------------------';
+		PRINT '==================='
+		PRINT 'LOADING BRONZE LAYER'
+		PRINT '==================='
 
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.crm_cust_info';
-		TRUNCATE TABLE bronze.crm_cust_info;
-		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
-		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-		WITH (
+		SET @batchstartdate = GETDATE()
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.data_dictionary'
+		TRUNCATE TABLE bronze.data_dictionary;
+		PRINT '>>>>>INSERT table bronze.data_dictionary'
+		BULK INSERT bronze.data_dictionary
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\data_dictionary.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.crm_prd_info';
-		TRUNCATE TABLE bronze.crm_prd_info;
-
-		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		WITH (
+		/*
+		===================
+		bronze.order_item_refunds
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.order_item_refunds'
+		TRUNCATE TABLE bronze.order_item_refunds;
+		PRINT '>>>>>INSERT table bronze.order_item_refunds'
+		BULK INSERT bronze.order_item_refunds
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\order_item_refunds.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.crm_sales_details';
-		TRUNCATE TABLE bronze.crm_sales_details;
-		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
-		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		WITH (
+		/*
+		===================
+		bronze.order_items
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.order_items'
+		TRUNCATE TABLE bronze.order_items;
+		PRINT '>>>>>INSERT table bronze.order_items'
+		BULK INSERT bronze.order_items
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\order_items.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-		PRINT '------------------------------------------------';
-		PRINT 'Loading ERP Tables';
-		PRINT '------------------------------------------------';
-		
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.erp_loc_a101';
-		TRUNCATE TABLE bronze.erp_loc_a101;
-		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-		WITH (
+
+		/*
+		===================
+		bronze.orders
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.orders'
+		TRUNCATE TABLE bronze.orders;
+		PRINT '>>>>>INSERT table bronze.orders'
+		BULK INSERT bronze.orders
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\orders.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.erp_cust_az12';
-		TRUNCATE TABLE bronze.erp_cust_az12;
-		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-		WITH (
+
+		/*
+		===================
+		bronze.products
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.products'
+		TRUNCATE TABLE bronze.products;
+		PRINT '>>>>>INSERT table bronze.products'
+		BULK INSERT bronze.products
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\products.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\DATA WAREHOUSE\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-		WITH (
+
+		/*
+		===================
+		bronze.website_pageviews
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.website_pageviews'
+		TRUNCATE TABLE bronze.website_pageviews;
+		PRINT '>>>>>INSERT table bronze.website_pageviews'
+		BULK INSERT bronze.website_pageviews
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\website_pageviews.csv'
+		WITH
+		(
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> -------------';
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
 
-		SET @batch_end_time = GETDATE();
-		PRINT '=========================================='
-		PRINT 'Loading Bronze Layer is Completed';
-        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-		PRINT '=========================================='
-	END TRY
-	BEGIN CATCH
+
+		/*
+		===================
+		bronze.website_sessions
+		===================
+		*/
+		SET @startdate = GETDATE()
+		PRINT '>>>>>TRUNCATE table bronze.website_sessions'
+		TRUNCATE TABLE bronze.website_sessions;
+		PRINT '>>>>>INSERT table bronze.website_sessions'
+		BULK INSERT bronze.website_sessions
+		FROM 'C:\DATA WAREHOUSE\MEDALLION ARCHITECTURE\Maven+Fuzzy+Factory\website_sessions.csv'
+		WITH
+		(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @enddate = GETDATE()
+		PRINT '>>>>>Duration: ' + CAST(DATEDIFF(SECOND ,@startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '=========================================================='
+
+		PRINT '====================='
+		PRINT 'Loading Bronze layer is Completed';
+		PRINT 'Total Load Duration: ' + CAST(DATEDIFF(second, @startdate, @enddate) as NVARCHAR) + ' seconds';
+		PRINT '====================='
+
+
+	COMMIT TRAN
+END TRY
+BEGIN CATCH
 		PRINT '=========================================='
 		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
 		PRINT 'Error Message' + ERROR_MESSAGE();
 		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
 		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
 		PRINT '=========================================='
-	END CATCH
-END
+	IF XACT_STATE() <> 0 ROLLBACK TRAN;
+    THROW;
+END CATCH
